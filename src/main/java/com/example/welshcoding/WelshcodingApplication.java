b3920176c24485ac9b39f799d25dbcf3c0d37cef
Jang-jiwon <66350779+Jang-jiwon@users.noreply.github.com>
2023-05-10 23:24:03 +0900

    first commit

---
+++ src/main/java/com/example/welshcoding/WelshcodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.welshcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelshcodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelshcodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/welshcoding/WelshcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welshcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelshcodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
