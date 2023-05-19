99817d7d5f8da8ade796f40384ca66a5659706d7
kdy <forever89829@naver.com>
2023-05-20 00:16:28 +0900

    Revert "Revert "no board of series UPDATE""
    This reverts commit 90457ae0d13fb17be80a60f8f15da4b89db7d2e0.

--- src/main/java/com/example/welshcoding/series/SeriesController.java
+++ src/main/java/com/example/welshcoding/series/SeriesController.java
@@ -20,7 +20,7 @@ public class SeriesController {
 
 	private final SeriesService seriesService;
 	
-//	@GetMapping("/kdy/series/{memberId}")
+	@GetMapping("/kdy/series/{memberId}")
 	public String seriesList(@PathVariable Long memberId, Model model) throws ParseException {
 		
 		List<SeriesListDTO> seriesList = seriesService.findSeriesAll(memberId);
@@ -49,10 +49,13 @@ public class SeriesController {
 		}
 		
 		List<BoardDTO> boardList = seriesService.findBoardsBySeries(seriesId);
+		if(boardList.size() == 0) {
+			model.addAttribute("boardsOfSeriesSize", false);
+		} else {
+			model.addAttribute("boardsOfSeriesSize", true);
+		}
 		model.addAttribute("boardList",boardList);
-		model.addAttribute("seriesName",boardList.get(0).getSeriesName());
-		
-		
+		model.addAttribute("seriesName",seriesService.findSeriesById(seriesId).getSeriesName());
 		return "series/seriesDetail";
 	}
 	@GetMapping("/kdy/series/{memberId}/{seriesId}/edit")
@@ -63,8 +66,13 @@ public class SeriesController {
 			) {
 		
 		List<BoardDTO> boardList = seriesService.findBoardsBySeries(seriesId);
+		if(boardList.size() == 0) {
+			model.addAttribute("boardsOfSeriesSize", false);
+		} else {
+			model.addAttribute("boardsOfSeriesSize", true);
+		}
 		model.addAttribute("boardList",boardList);
-		model.addAttribute("seriesName",boardList.get(0).getSeriesName());
+		model.addAttribute("seriesName",seriesService.findSeriesById(seriesId).getSeriesName());
 		return "series/seriesUpdate";
 	}
 	@GetMapping("/kdy/series/{memberId}/{seriesId}/delete")
